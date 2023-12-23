56d98e07f84510412ef567641741fab6bb3a4cb9
Monfared.Amir <Monfared.Amir@GIG001P10612>
2023-12-23 14:45:08 +0330

    Implement registration services all at once

--- ProcessMining.EndPoint.API/Extensions/ProcessMiningServiceExtension.cs
+++ ProcessMining.EndPoint.API/Extensions/ProcessMiningServiceExtension.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProcessMining.Core.ApplicationService.Queries;
 using ProcessMining.Core.ApplicationService.Services;
+using ProcessMining.Core.Domain.Attributes;
 using ProcessMining.Infra.EntityFramework.DbContextes;
+using ProcessMining.Infra.Tools.Reflections;
 
 namespace ProcessMining.EndPoint.API.Extensions
 {
@@ -17,9 +19,20 @@ namespace ProcessMining.EndPoint.API.Extensions
 
         public static IServiceCollection AddProcessMiningServices(this IServiceCollection services)
         {
+            // Register base services
             services.AddTransient(typeof(IBaseService<>), typeof(BaseService<>));
-            services.AddTransient<IUserService, UserService>();
-            services.AddTransient<IAuthenticationService, AuthenticationService>();
+
+            // Get all services corresponding to Registration Required Attribute
+            var repositoryTypes = Assemblies.GetServices("ProcessMining.Core.ApplicationService", typeof(RegistrationRequiredAttribute));
+
+            // Register each service
+            foreach (var repositoryType in repositoryTypes)
+            {
+                var repositoryInterface = repositoryType.GetInterfaces().Where(i => !i.IsGenericType).FirstOrDefault();
+                services.AddTransient(repositoryInterface, repositoryType);
+            }
+
+            // Return extension method value
             return services;
         }
     }
---
+++ ProcessMining.Infra.Tools/Reflections/Assemblies.cs
@@ -0,0 +1,37 @@
+﻿using ProcessMining.Core.Domain.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMining.Infra.Tools.Reflections
+{
+    public static class Assemblies
+    {
+        public static List<Type> GetServices(string assemblyName, Type attribute)
+        {
+            var services = new List<Type>();
+
+            var loadedAssemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
+            var loadedPaths = loadedAssemblies.Select(a => a.Location).ToArray();
+            var referencedPaths = System.IO.Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll");
+            var toLoad = referencedPaths.Where(r => !loadedPaths.Contains(r, StringComparer.InvariantCultureIgnoreCase)).ToList();
+            toLoad.ForEach(path => loadedAssemblies.Add(AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(path))));
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList().Where(a => a.FullName.Contains(assemblyName));
+
+            foreach (var assembly in assemblies)
+            {
+                var types = assembly.GetTypes().Where(c => c.IsClass && c.IsDefined(attribute));
+                foreach (var type in types)
+                {
+                    services.Add(type);
+                }
+            }
+
+            return services;
+
+        }
+    }
+}
