a7cda867e44f766829e95a8c62def854f02e6e78
Monfared.Amir <Monfared.Amir@GIG001P10612>
2023-12-25 11:29:24 +0330

    Implement token based authorization, it works and i'm happy :)

--- ProcessMining.EndPoint.API/Program.cs
+++ ProcessMining.EndPoint.API/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using ProcessMining.Core.Domain.Models;
 using ProcessMining.EndPoint.API.Extensions;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace ProcessMining.EndPoint.API
 {
@@ -18,9 +22,50 @@ namespace ProcessMining.EndPoint.API
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationConfiguration.AccessTokenSecret)),
+                    ValidIssuer = authenticationConfiguration.Issuer,
+                    ValidAudience = authenticationConfiguration.Audience,
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                };
+            });
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition(
+                    name: JwtBearerDefaults.AuthenticationScheme,
+                    securityScheme: new OpenApiSecurityScheme()
+                    {
+                        Name = "Authorization",
+                        Description = "Enter the Bearer Authorization : `Bearer Generated-JWT-Token`",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer"
+                    }
+                    );
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = JwtBearerDefaults.AuthenticationScheme
+                            }
+                    }
+                        ,new string[] {}
+                    }
+                });
+            });
 
             // Add DbContext
             builder.Services.AddProcessMiningDbContext();
