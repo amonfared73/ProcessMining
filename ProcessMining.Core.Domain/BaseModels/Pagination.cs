8368a6598c14cf4261e9075db282c5155a07ceb2
monfared.amir <Monfared.Amir@Golrang.com>
2023-12-27 07:36:45 +0330

    Implement paged result viewmodels

---
+++ ProcessMining.Core.Domain/BaseModels/Pagination.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMining.Core.Domain.BaseModels
+{
+    public class Pagination
+    {
+        public int CurrentPage { get; set; } = 1;
+        public int PerPage { get; set; } = 10;
+        public long? TotalCount { get; set; }
+        public int? PageCount => (TotalCount > 0) ? ((int)Math.Ceiling((double)(TotalCount / ((PerPage > 0) ? PerPage : 10)).Value)) : 0;
+    }
+}
---
+++ ProcessMining.Core.Domain/BaseViewModels/PagedResultViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ProcessMining.Core.Domain.BaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMining.Core.Domain.BaseViewModels
+{
+    public class PagedResultViewModel<T> where T : DomainObject
+    {
+        public Pagination Pagination { get; set; }
+        IEnumerable<T> Data { get; set; }
+    }
+}
