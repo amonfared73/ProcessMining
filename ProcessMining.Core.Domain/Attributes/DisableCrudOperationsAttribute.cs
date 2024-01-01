adfabd5abbadb939efb40b6b7b186cd98bc5ea31
Monfared.Amir <Monfared.Amir@GIG001P10612>
2024-01-01 13:26:49 +0330

    Add Attribute Targets on DisableCrudOperationsAttribute

--- ProcessMining.Core.Domain/Attributes/DisableCrudOperationsAttribute.cs
+++ ProcessMining.Core.Domain/Attributes/DisableCrudOperationsAttribute.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProcessMining.Core.Domain.Attributes
 {
+    [AttributeUsage(AttributeTargets.Class)]
     public class DisableCrudOperationsAttribute : Attribute
     {
     }
