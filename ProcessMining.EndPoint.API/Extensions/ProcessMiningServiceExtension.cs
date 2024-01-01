16728b4457fa4d7ba932423270076c2136c9caa1
Monfared.Amir <Monfared.Amir@GIG001P10612>
2024-01-01 10:16:20 +0330

    Implement encapsulation on WebApplicationBuilder in Program.cs

--- ProcessMining.EndPoint.API/Extensions/ProcessMiningServiceExtension.cs
+++ ProcessMining.EndPoint.API/Extensions/ProcessMiningServiceExtension.cs
@@ -1,18 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ProcessMining.Core.ApplicationService.Queries;
 using ProcessMining.Core.ApplicationService.Services;
 using ProcessMining.Core.ApplicationService.Services.Authenticators;
 using ProcessMining.Core.ApplicationService.TokenGenerators;
 using ProcessMining.Core.ApplicationService.TokenValidators;
 using ProcessMining.Core.Domain.Attributes;
+using ProcessMining.Core.Domain.Models;
 using ProcessMining.Infra.EntityFramework.DbContextes;
 using ProcessMining.Infra.Tools.Reflections;
+using System.Text;
 
 namespace ProcessMining.EndPoint.API.Extensions
 {
     public static class ProcessMiningServiceExtension
     {
-        public static IServiceCollection AddProcessMiningDbContext(this IServiceCollection services, string connectionString)
+        private static IServiceCollection AddProcessMiningDbContext(this IServiceCollection services, string connectionString)
         {
             //var connectionString = "Data Source=ProcessMining.db";
             services.AddDbContext<ProcessMiningDbContext>(options => options.UseSqlite(connectionString), optionsLifetime: ServiceLifetime.Singleton);
@@ -20,7 +25,7 @@ namespace ProcessMining.EndPoint.API.Extensions
             return services;
         }
 
-        public static IServiceCollection AddAccessTokenGenerator(this IServiceCollection services)
+        private static IServiceCollection AddAccessTokenGenerator(this IServiceCollection services)
         {
             services.AddSingleton<AccessTokenGenerator>();
             services.AddSingleton<RefreshTokenGenerator>();
@@ -30,7 +35,7 @@ namespace ProcessMining.EndPoint.API.Extensions
             return services;
         }
 
-        public static IServiceCollection AddProcessMiningServices(this IServiceCollection services)
+        private static IServiceCollection AddProcessMiningServices(this IServiceCollection services)
         {
             // Register base services
             services.AddTransient(typeof(IBaseService<>), typeof(BaseService<>));
@@ -48,6 +53,94 @@ namespace ProcessMining.EndPoint.API.Extensions
 
             // Return extension method value
             return services;
+        }
+
+        public static WebApplicationBuilder AddProcessMining(this WebApplicationBuilder builder, out WebApplication app)
+        {
+            // Authentication configuration
+            var authenticationConfiguration = new AuthenticationConfiguration();
+            var configuration = builder.Services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+            var connectionString = configuration.GetConnectionString("sqlite");
+            configuration.Bind("Authentication", authenticationConfiguration);
+            builder.Services.AddSingleton(authenticationConfiguration);
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationConfiguration.AccessTokenSecret)),
+                    ValidIssuer = authenticationConfiguration.Issuer,
+                    ValidAudience = authenticationConfiguration.Audience,
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition(
+                    name: JwtBearerDefaults.AuthenticationScheme,
+                    securityScheme: new OpenApiSecurityScheme()
+                    {
+                        Name = "Authorization",
+                        Description = "Enter the Bearer Authorization : `Bearer Generated-JWT-Token`",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer"
+                    }
+                    );
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = JwtBearerDefaults.AuthenticationScheme
+                            }
+                    }
+                        ,new string[] {}
+                    }
+                });
+            });
+
+            // Add DbContext
+            builder.Services.AddProcessMiningDbContext(connectionString);
+
+            // Add Access Token Generator
+            builder.Services.AddAccessTokenGenerator();
+
+            // Add Services
+            builder.Services.AddProcessMiningServices();
+
+            app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(o =>
+                {
+                    o.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            return builder;
         }
     }
 }
--- ProcessMining.EndPoint.API/Program.cs
+++ ProcessMining.EndPoint.API/Program.cs
@@ -1,9 +1,4 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using ProcessMining.Core.Domain.Models;
 using ProcessMining.EndPoint.API.Extensions;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Text;
 
 namespace ProcessMining.EndPoint.API
 {
@@ -12,92 +7,7 @@ namespace ProcessMining.EndPoint.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Authentication configuration
-            var authenticationConfiguration = new AuthenticationConfiguration();
-            var configuration = builder.Services.BuildServiceProvider().GetRequiredService<IConfiguration>();
-            var connectionString = configuration.GetConnectionString("sqlite");
-            configuration.Bind("Authentication", authenticationConfiguration);
-            builder.Services.AddSingleton(authenticationConfiguration);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
-            {
-                o.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationConfiguration.AccessTokenSecret)),
-                    ValidIssuer = authenticationConfiguration.Issuer,
-                    ValidAudience = authenticationConfiguration.Audience,
-                    ValidateIssuerSigningKey = true,
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ClockSkew = TimeSpan.Zero
-                };
-            });
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.AddSecurityDefinition(
-                    name: JwtBearerDefaults.AuthenticationScheme,
-                    securityScheme: new OpenApiSecurityScheme()
-                    {
-                        Name = "Authorization",
-                        Description = "Enter the Bearer Authorization : `Bearer Generated-JWT-Token`",
-                        In = ParameterLocation.Header,
-                        Type = SecuritySchemeType.ApiKey,
-                        Scheme = "Bearer"
-                    }
-                    );
-                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                {
-                    {
-                        new OpenApiSecurityScheme()
-                        {
-                            Reference = new OpenApiReference()
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = JwtBearerDefaults.AuthenticationScheme
-                            }
-                    }
-                        ,new string[] {}
-                    }
-                });
-            });
-
-            // Add DbContext
-            builder.Services.AddProcessMiningDbContext(connectionString);
-
-            // Add Access Token Generator
-            builder.Services.AddAccessTokenGenerator();
-
-            // Add Services
-            builder.Services.AddProcessMiningServices();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(o =>
-                {
-                    o.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
-                });
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
+            builder.AddProcessMining(out WebApplication app);
             app.Run();
         }
     }
