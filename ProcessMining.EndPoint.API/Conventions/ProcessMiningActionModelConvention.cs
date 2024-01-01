f7d26e15c968d93bc6fc9b85c5647966d74cadcf
Monfared.Amir <Monfared.Amir@GIG001P10612>
2024-01-01 15:45:13 +0330

    Introduce Action and Controller Conventions

---
+++ ProcessMining.EndPoint.API/Conventions/ProcessMiningActionModelConvention.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace ProcessMining.EndPoint.API.Conventions
+{
+    public class ProcessMiningActionModelConvention : IActionModelConvention
+
+    {
+        public void Apply(ActionModel action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProcessMining.EndPoint.API/Conventions/ProcessMiningControllerModelConvention.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace ProcessMining.EndPoint.API.Conventions
+{
+    public class ProcessMiningControllerModelConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
