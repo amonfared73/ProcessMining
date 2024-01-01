a00017953ffb0d0f37346e185cb18c7ff847aa9a
Monfared.Amir <Monfared.Amir@GIG001P10612>
2024-01-01 13:22:33 +0330

    Introduce Disable Crud Operations Attribute

--- ProcessMining.EndPoint.API/Controllers/AuthenticationController.cs
+++ ProcessMining.EndPoint.API/Controllers/AuthenticationController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ProcessMining.Core.ApplicationService.Services;
+using ProcessMining.Core.Domain.Attributes;
 using ProcessMining.Core.Domain.BaseViewModels;
 using ProcessMining.Core.Domain.DTOs;
 using ProcessMining.Core.Domain.Models;
@@ -17,31 +18,6 @@ namespace ProcessMining.EndPoint.API.Controllers
         public AuthenticationController(IAuthenticationService service) : base(service)
         {
             _service = service;
-        }
-        [NonAction]
-        public override Task<PagedResultViewModel<Authentication>> GetAllAsync(BaseRequestViewModel request)
-        {
-            return base.GetAllAsync(request);
-        }
-        [NonAction]
-        public override Task<SingleResultViewModel<Authentication>> GetByIdAsync(int id)
-        {
-            return base.GetByIdAsync(id);
-        }
-        [NonAction]
-        public override Task<SingleResultViewModel<Authentication>> InsertAsync(Authentication entity)
-        {
-            return base.InsertAsync(entity);
-        }
-        [NonAction]
-        public override Task<SingleResultViewModel<Authentication>> UpdateAsync(Authentication entity)
-        {
-            return base.UpdateAsync(entity);
-        }
-        [NonAction]
-        public override Task<SingleResultViewModel<Authentication>> DeleteAsync(int id)
-        {
-            return base.DeleteAsync(id);
         }
     }
 }
