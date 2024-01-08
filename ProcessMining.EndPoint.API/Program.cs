85dc0d032be9f95e5c83b81ff436eef8dba4571b
Monfared.Amir <Monfared.Amir@GIG001P10612>
2024-01-08 07:12:03 +0330

    update program.cs file

--- ProcessMining.EndPoint.API/Program.cs
+++ ProcessMining.EndPoint.API/Program.cs
@@ -1,14 +1,4 @@
 using ProcessMining.EndPoint.API.Extensions;
-
-namespace ProcessMining.EndPoint.API
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.AddProcessMining(out WebApplication app);
-            app.Run();
-        }
-    }
-}
+var builder = WebApplication.CreateBuilder(args);
+builder.AddProcessMining(out WebApplication app);
+app.Run();
