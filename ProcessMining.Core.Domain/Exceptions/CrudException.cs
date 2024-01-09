9599db77a8d8d525acfd9172d7eb0f67e50db565
Amir Monfared <amonfared73@gmail.com>
2024-01-09 19:45:23 +0330

    Introduce generic crud excewption

---
+++ ProcessMining.Core.Domain/Exceptions/CrudException.cs
@@ -0,0 +1,24 @@
+﻿using ProcessMining.Core.Domain.BaseModels;
+
+namespace ProcessMining.Core.Domain.Exceptions
+{
+    public class CrudException<T> : Exception where T : DomainObject
+    {
+        public T Entity { get; }
+
+        public CrudException(T entity)
+        {
+            Entity = entity;
+        }
+
+        public CrudException(string? message, T entity) : base(message)
+        {
+            Entity = entity;
+        }
+
+        public CrudException(string? message, Exception? innerException, T entity) : base(message, innerException)
+        {
+            Entity = entity;
+        }
+    }
+}
