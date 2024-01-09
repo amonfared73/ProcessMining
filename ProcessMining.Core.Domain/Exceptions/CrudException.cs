8ad8e43ef31217023d7827bf3912c93629dd7ef4
Amir Monfared <amonfared73@gmail.com>
2024-01-09 19:46:53 +0330

    Add base operation parameter to generic exception

--- ProcessMining.Core.Domain/Exceptions/CrudException.cs
+++ ProcessMining.Core.Domain/Exceptions/CrudException.cs
@@ -1,24 +1,29 @@
 ﻿using ProcessMining.Core.Domain.BaseModels;
+using ProcessMining.Core.Domain.Enums;
 
 namespace ProcessMining.Core.Domain.Exceptions
 {
     public class CrudException<T> : Exception where T : DomainObject
     {
         public T Entity { get; }
+        public BaseOperations BaseOperation { get; }
 
-        public CrudException(T entity)
+        public CrudException(T entity, BaseOperations baseOperation)
         {
             Entity = entity;
+            BaseOperation = baseOperation;
         }
 
-        public CrudException(string? message, T entity) : base(message)
+        public CrudException(string? message, T entity, BaseOperations baseOperation) : base(message)
         {
             Entity = entity;
+            BaseOperation = baseOperation;
         }
 
-        public CrudException(string? message, Exception? innerException, T entity) : base(message, innerException)
+        public CrudException(string? message, Exception? innerException, T entity, BaseOperations baseOperation) : base(message, innerException)
         {
             Entity = entity;
+            BaseOperation = baseOperation;
         }
     }
 }
